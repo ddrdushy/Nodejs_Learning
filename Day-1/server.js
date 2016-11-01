825d53b2840f8cbf4882f127e92da4e73d4857b0
ddrdushy <dushy2009@gmail.com>
2016-11-01 16:24:24 +0530

    logging the incoming request by using url property of request object

--- Day-1/server.js
+++ Day-1/server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request,response){
+    console.log(`Request Coming from: ${request.url}`);
     response.end("Hello World!");
 }).listen(3000,"127.0.0.1",()=>{
    console.log("Server is listening on localhost port 3000");
