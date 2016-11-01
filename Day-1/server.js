e0cb220ca305c2ca06dbf8d844c336506deaf89b
ddrdushy <dushy2009@gmail.com>
2016-11-01 16:30:36 +0530

    if conndition added for the request to redirect to the specific page

--- Day-1/server.js
+++ Day-1/server.js
@@ -2,6 +2,11 @@ var http = require("http");
 
 http.createServer(function(request,response){
     console.log(`Request Coming from: ${request.url}`);
+
+    if(request.url.startsWith("/static/")){
+        return ;
+    }
+
     response.end("Hello World!");
 }).listen(3000,"127.0.0.1",()=>{
    console.log("Server is listening on localhost port 3000");
