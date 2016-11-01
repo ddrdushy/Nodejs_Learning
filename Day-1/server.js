fbaecde6e4d239fa33186f5dfcff297f382b69cf
ddrdushy <dushy2009@gmail.com>
2016-11-01 16:44:11 +0530

    if not error serve data

--- Day-1/server.js
+++ Day-1/server.js
@@ -8,9 +8,10 @@ http.createServer(function(request,response){
         fs.readFile(request.url.substr(1),(error,data)=>{
             if(error){
                 console.log("Error: file not found "+ error);
+                response.end();
                 response.statusCode=404;
             }
-            
+            response.end(data);
         });
         return ;
     }
