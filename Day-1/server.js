a42eb0ef8223b907364d9811b3878e2bffbd6c25
ddrdushy <dushy2009@gmail.com>
2016-11-01 16:17:35 +0530

    response is added with the String 'Hello World'

--- Day-1/server.js
+++ Day-1/server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request,response){
-
+    response.end("Hello World!");
 }).listen(3000,"127.0.0.1",()=>{
    console.log("Server is listening on localhost port 3000");
 });
