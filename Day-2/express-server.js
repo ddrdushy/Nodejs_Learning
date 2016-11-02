1780b16357fa974ce710f8625e5e4cb397a1c844
ddrdushy <dushy2009@gmail.com>
2016-11-02 21:52:35 +0530

    Express.js framework added to the module and express.js server is created

---
+++ Day-2/express-server.js
@@ -0,0 +1,10 @@
+var express=require("express");
+var app=express();
+
+app.get("/",function(req,res){
+    res.end("Hello World");
+});
+
+app.listen(3000,'127.0.0.1',()=>{
+    console.log("Server is Listening on port 3000");
+});
