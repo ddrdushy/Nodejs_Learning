a7f3137496dcf41f01e2028555c9aef57660a2fa
ddrdushy <dushy2009@gmail.com>
2016-11-07 22:12:34 +0530

    Title variable is passed to the jade view

--- Day-7/app.js
+++ Day-7/app.js
@@ -1,14 +1,16 @@
 var express=require("express");
 var app=express();
 
+app.set("views","./views");
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
 app.get("/",(req,res)=>{
-    res.render("index.jade");
+    res.render("index.jade",{title:"Home"});
 });
 app.get("/admin/rooms",(req,res)=>{
-    res.render("rooms.jade");
+    res.render("rooms.jade",{title:"Admin Rooms"});
 });
 
 app.listen(3000,function(){
