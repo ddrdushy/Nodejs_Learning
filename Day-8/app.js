dfd02c5f64028f88dfc573a575b113955413ddf6
ddrdushy <dushy2009@gmail.com>
2016-11-08 21:48:58 +0530

    new rooute is added to the add room task

--- Day-8/app.js
+++ Day-8/app.js
@@ -1,19 +1,26 @@
 var express=require("express");
 var app=express();
 var rooms=require("./data/rooms.json");
+
 app.set("views","./views");
 app.set('view engine','jade');
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
 app.get("/",(req,res)=>{
     res.render("index",{title:"Home"});
 });
+
 app.get("/admin/rooms",(req,res)=>{
     res.render("rooms",{
         title:"Admin Rooms",
         rooms: rooms
     });
+});
+
+app.get("/admin/rooms/add",(req,res)=>{
+    res.render("add");
 });
 
 app.listen(3000,function(){
