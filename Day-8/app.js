2a0a3bff1317cd7e097e337d49e4928b201ea95b
ddrdushy <dushy2009@gmail.com>
2016-11-09 00:27:32 +0530

    Added the edit function code

--- Day-8/app.js
+++ Day-8/app.js
@@ -3,6 +3,8 @@ var app=express();
 var rooms=require("./data/rooms.json");
 var bodyParser=require("body-parser");
 var uuid=require("node-uuid");
+var _=require("lodash");
+
 
 app.set("views","./views");
 app.set('view engine','jade');
@@ -38,10 +40,25 @@ app.post("/admin/rooms/add",(req,res)=>{
 app.get("/admin/rooms/delete/:id",(req,res)=>{
     var roomId=req.params.id;
     rooms=rooms.filter(r => r.id !== roomId);
-    
+
     res.redirect("/admin/rooms");
 });
 
+app.post("/admin/rooms/edit/:id",(req,res)=>{
+    var roomId=req.params.id;
+    var room=_.find(rooms,r => r.id === roomId);
+
+    room.name=req.body.name;
+
+    res.redirect("/admin/rooms");
+});
+
+app.get("/admin/rooms/edit/:id",(req,res)=>{
+    var roomId=req.params.id;
+    var room=_.find(rooms,r => r.id === roomId);
+
+    res.render("edit",{room});
+});
 app.listen(3000,function(){
     console.log("Chat app listening on port 3000");
 });
