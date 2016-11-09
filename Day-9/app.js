8400cedb806af01625f1afd335d3f7106e25afda
ddrdushy <dushy2009@gmail.com>
2016-11-09 21:32:23 +0530

    used bodyparser for json requests

--- Day-9/app.js
+++ Day-9/app.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/jquery/dist"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 
 app.get("/",(req,res)=>{
