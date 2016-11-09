259c64e6b41a4359bd114ef75233ba0235f94f17
ddrdushy <dushy2009@gmail.com>
2016-11-09 18:01:22 +0530

    Understanding the middleware functions

--- Day-9/app.js
+++ Day-9/app.js
@@ -9,6 +9,10 @@ app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use((req,res,next)=>{
+   console.log(`Incomming Request: ${req.url}`);
+});
+
 app.get("/",(req,res)=>{
     res.render("index",{title:"Home"});
 });
