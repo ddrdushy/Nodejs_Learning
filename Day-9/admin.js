8abd732f3b2a8fdc35627f9d75c4252d85335721
ddrdushy <dushy2009@gmail.com>
2016-11-09 16:02:02 +0530

    Router added

--- Day-9/admin.js
+++ Day-9/admin.js
@@ -1,36 +1,39 @@
 var uuid=require("node-uuid");
 var _=require("lodash");
+var express=require("express");
 var rooms=require("./data/rooms.json");
 
-module.exports = function(app) {
-    app.get("/admin/rooms", (req, res)=> {
+var router=express.Router();
+module.exports=router;
+
+router.get("/rooms", (req, res)=> {
         res.render("rooms", {
             title: "Admin Rooms",
             rooms: rooms
         });
     });
 
-    app.get("/admin/rooms/add", (req, res)=> {
+router.get("/rooms/add", (req, res)=> {
         res.render("add");
     });
 
-    app.post("/admin/rooms/add", (req, res)=> {
+router.post("/rooms/add", (req, res)=> {
         var room = {
             name: req.body.name,
             id: uuid.v4()
         };
         rooms.push(room);
-        res.redirect("/admin/rooms");
+        res.redirect(req.baseUrl+"/rooms");
     });
 
-    app.get("/admin/rooms/delete/:id", (req, res)=> {
+router.get("/rooms/delete/:id", (req, res)=> {
         var roomId = req.params.id;
         rooms = rooms.filter(r => r.id !== roomId);
 
-        res.redirect("/admin/rooms");
+        res.redirect(req.baseUrl+"/rooms");
     });
 
-    app.post("/admin/rooms/edit/:id", (req, res)=> {
+router.post("/rooms/edit/:id", (req, res)=> {
         var roomId = req.params.id;
         var room = _.find(rooms, r => r.id === roomId);
         if (!room) {
@@ -39,10 +42,10 @@ module.exports = function(app) {
         }
         room.name = req.body.name;
 
-        res.redirect("/admin/rooms");
+        res.redirect(req.baseUrl+"/rooms");
     });
 
-    app.get("/admin/rooms/edit/:id", (req, res)=> {
+router.get("/rooms/edit/:id", (req, res)=> {
         var roomId = req.params.id;
         var room = _.find(rooms, r => r.id === roomId);
         if (!room) {
@@ -51,4 +54,3 @@ module.exports = function(app) {
         }
         res.render("edit", {room});
     });
-}
