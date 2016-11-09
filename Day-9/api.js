3e88eae765355760bb63cd6e657f7dcd60b7bb70
ddrdushy <dushy2009@gmail.com>
2016-11-09 21:03:48 +0530

    room messages returned to the api request

--- Day-9/api.js
+++ Day-9/api.js
@@ -1,9 +1,28 @@
 var express=require("express");
 var rooms=require("./data/rooms.json");
+var messages=require("./data/messages.json");
+var _=require("lodash");
 
 var router=express.Router();
 module.exports=router;
 
 router.get("/rooms",(req,res)=>{
     res.json(rooms);
+});
+
+router.get("/rooms/:roomid/messages",(req,res)=>{
+    var roomId=req.params.roomid;
+    var roomMessages=messages
+    .filter(m => m.roomId === roomId);
+
+    var room = _.find(rooms, r => r.id === roomId);
+    if (!room) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.json({
+        room:room,
+        messages:roomMessages
+    });
 });
