6ceff0f4835f4d11313dd80bbfd49e26c697df38
ddrdushy <dushy2009@gmail.com>
2016-11-11 21:22:26 +0530

    Created the logging.js for get the logs

---
+++ Day-11/logging.js
@@ -0,0 +1,5 @@
+
+var fs=require("fs");
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
+module.exports = require("morgan")("combined",{stream:accessLogStream});
