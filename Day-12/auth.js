05302f84d4e4142249f81428b341ba19648d7311
ddrdushy <dushy2009@gmail.com>
2016-11-12 20:11:55 +0530

    added a snippet of code to bypass the login in the development enviornment

--- Day-12/auth.js
+++ Day-12/auth.js
@@ -1,10 +1,20 @@
 var express=require("express");
 var passport=require("passport");
+var users=require("./data/users.json");
 
 var router=express.Router();
 module.exports=router;
 
 router.get("/login",(req,res)=>{
+    if(req.app.get("env") === "development"){
+        var user=users[0];
+
+        req.logIn(user,(err)=>{
+           if(err){return next();}
+            return res.redirect("/");
+        });
+        return;
+    }
     res.render("login");
 });
 
